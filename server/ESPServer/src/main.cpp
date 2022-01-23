6d059536cf899affc50b8bf79aad9213aff5500e
Calin Cocan <calin.cocan@datagridsoftware.com>
2022-01-23 22:09:22 +0200

    update internal LED default state to HIGH for NO light mode

--- server/ESPServer/src/main.cpp
+++ server/ESPServer/src/main.cpp
@@ -158,13 +158,13 @@ void handleChangeStateGate()
   {
     return server.requestAuthentication();
   }
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
   delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
   delay(500); 
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
   delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
   delay(500); 
   String message = STATE_CHANGED_GATE_RESP;
   server.send(200, "application/json", message);
@@ -176,9 +176,9 @@ void handleChageStateDoor()
   {
     return server.requestAuthentication();
   }
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
   delay(1000); 
   String message = STATE_CHANGED_DOOR_RESP;
   server.send(200, "application/json", message);
