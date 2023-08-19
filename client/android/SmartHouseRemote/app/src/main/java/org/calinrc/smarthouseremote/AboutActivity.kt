4736c412c75dfa7ce00169bc4532463b478e97fe
Calin Cocan <calin.cocan@datagridsoftware.com>
2023-08-19 14:07:52 +0300

    feat: add version in about dialog

--- client/android/SmartHouseRemote/app/src/main/java/org/calinrc/smarthouseremote/AboutActivity.kt
+++ client/android/SmartHouseRemote/app/src/main/java/org/calinrc/smarthouseremote/AboutActivity.kt
@@ -1,12 +1,25 @@
 package org.calinrc.smarthouseremote
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        var versionName = ""
+        try {
+            val packageInfo = packageManager.getPackageInfo(packageName, 0)
+            versionName = "Version: " + packageInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        val tv:TextView = findViewById(R.id.textViewVersion)
+        tv.text = versionName
+
     }
 }
